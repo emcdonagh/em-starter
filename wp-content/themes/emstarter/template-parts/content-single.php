cfe8616d506e8f9bf7f1cdbbed73f782522f5f1f
erin mcdonagh <erinemcdonagh@hotmail.com>
2017-12-10 20:31:19 -0800

    adds newest database, no-sidebar setup working... has-sidebar creating issues on main page.

--- wp-content/themes/emstarter/template-parts/content-single.php
+++ wp-content/themes/emstarter/template-parts/content-single.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * Template part for displaying posts
+ * Template part for displaying posts.
  *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ * @link https://codex.wordpress.org/Template_Hierarchy
  *
  * @package emstarter
  */
@@ -11,83 +11,70 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-	  <?php //add categories
- emstarter_the_category_list();
-        ?>
-
+		<?php emstarter_the_category_list(); ?>
 		<?php
-		if ( is_singular() ) :
+		if ( is_single() ) :
 			the_title( '<h1 class="entry-title">', '</h1>' );
 		else :
 			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
 		endif;
 
-		if ( is_active_sidebar('sidebar-1') ) : ?>
+		if ( is_active_sidebar( 'sidebar-1' ) ) : ?>
 		<div class="entry-meta">
 			<?php emstarter_posted_on(); ?>
 		</div><!-- .entry-meta -->
 		<?php
 		endif; ?>
 	</header><!-- .entry-header -->
-<section class="post-content">
 
-<?php
-if ( !is_active_sidebar('sidebar-1') ) : ?>
-<div class="post-content__wrap">
-<div class="entry-meta">
-	<?php emstarter_posted_on(); ?>
-</div><!-- .entry-meta -->
-<div class="post-content__body">
-<?php
-endif;
-
-?>
-
-	<div class="entry-content">
+	<section class="post-content">
+		
 		<?php
-			the_content( sprintf(
-				wp_kses(
-					/* translators: %s: Name of current post. Only visible to screen readers */
-					__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'emstarter' ),
-					array(
-						'span' => array(
-							'class' => array(),
-						),
-					)
-				),
-				get_the_title()
-			) );
+		if ( !is_active_sidebar( 'sidebar-1' ) ) : ?>
+		<div class="post-content__wrap">
+			<div class="entry-meta">
+				<?php emstarter_posted_on(); ?>
+			</div><!-- .entry-meta -->
+			<div class="post-content__body">
+		<?php
+		endif; ?>
+		
+		<div class="entry-content">
+			<?php
+				the_content( sprintf(
+					/* translators: %s: Name of current post. */
+					wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'emstarter' ), array( 'span' => array( 'class' => array() ) ) ),
+					the_title( '<span class="screen-reader-text">"', '"</span>', false )
+				) );
 
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'emstarter' ),
-				'after'  => '</div>',
-			) );
+				wp_link_pages( array(
+					'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'emstarter' ),
+					'after'  => '</div>',
+				) );
+			?>
+		</div><!-- .entry-content -->
+
+		<footer class="entry-footer">
+			<?php emstarter_entry_footer(); ?>
+		</footer><!-- .entry-footer -->
+
+		<?php
+		if ( !is_active_sidebar( 'sidebar-1' ) ) : ?>
+			</div><!-- .post-content__body -->
+		</div><!-- .post-content__wrap -->
+		<?php endif; ?>
+		
+		<?php
+		emstarter_post_navigation();
+
+		// If comments are open or we have at least one comment, load up the comment template.
+		if ( comments_open() || get_comments_number() ) :
+			comments_template();
+		endif;
 		?>
-	</div><!-- .entry-content -->
-
-	<footer class="entry-footer">
-		<?php emstarter_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
-
-
-	<?php
-if ( !is_active_sidebar('sidebar-1') ) : ?>
-</div> <!-- .post-content__body -->
-</div> <!-- .post-content__wrap -->
-<?php 
-endif;
-?>
-<?php 
-	emstarter_post_navigation();
+	</section><!-- .post-content -->
 	
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif;
-?>
-
-</section>
-
-	<?php get_sidebar(); ?>
-
-</article><!-- #post-<?php the_ID(); ?> -->
+	<?php
+	get_sidebar();
+	?>
+</article><!-- #post-## -->
